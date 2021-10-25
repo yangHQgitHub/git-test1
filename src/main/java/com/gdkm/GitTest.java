5c032fbd199461db6095031debb3116b91079125
yhq <180@qq.com>
2021-10-26 01:54:21 +0800

    branch2commit

--- src/main/java/com/gdkm/GitTest.java
+++ src/main/java/com/gdkm/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("pushtest");
+        System.out.println("branch2commit");
     }
 }
