74af7495c73d2ea0fbfe81f20ffadaaf329a1130
yangHQgitHub <93076321+yangHQgitHub@users.noreply.github.com>
2021-10-26 02:05:54 +0800

    githubcommit2

--- src/main/java/com/gdkm/GitTest.java
+++ src/main/java/com/gdkm/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("pushtest");
         System.out.println("branch2commit");
+       System.out.println("github");
     }
 }
